bed6264d3409fcbab0c6956ec53e0c64ac285a2f
Bernhard Schweighofer <bernhard.schweighofer@tugraz.at>
2025-06-29 19:50:33 +0200

    some "initial" code: Read opts, set up a (single) keybinding in insert mode to call the convert_current_line function and "display" this line on screen :-)

--- lua/text2unicode/init.lua
+++ lua/text2unicode/init.lua
@@ -1,5 +1,35 @@
 local text2unicode = {}
 
-function text2unicode.setup(opts) end
+local opts
+local to_unicode_map
+local from_unicode_map
+
+local function transform_text(text)
+	vim.print("Transforming: ", text)
+	return text
+end
+
+function text2unicode.convert_current_line()
+	local old_text = vim.api.nvim_get_current_line()
+	local new_text = transform_text(old_text)
+	vim.api.nvim_set_current_line(new_text)
+end
+
+function text2unicode.setup(opts_arg)
+	opts = opts_arg or {}
+	opts.DEBUG = opts.DEBUG or false
+	opts.leader = opts.leader or "🌠"
+	if opts.DEBUG then
+		vim.print("text2unicode: opts = ", opts)
+	end
+
+	-- when in insert mode press <C-v> and then your desired key, so you should
+	-- see if it is already mapped, or use ":imap keycode" to check...
+	vim.keymap.set("i", "<M-l>", function()
+		text2unicode.convert_current_line()
+	end, {
+		desc = "text2unicode: Transform abbreviations in current line",
+	})
+end
 
 return text2unicode
