ad296d6e7558c779ba1fff393dfa1f87a360a95d
Bernhard Schweighofer <bernhard.schweighofer@tugraz.at>
2025-06-29 21:24:40 +0200

    load and process the abbreviation/unicode characters table

--- lua/text2unicode/init.lua
+++ lua/text2unicode/init.lua
@@ -1,8 +1,43 @@
 local text2unicode = {}
 
 local opts
-local to_unicode_map
-local from_unicode_map
+local to_unicode_map = {}
+local from_unicode_map = {}
+
+local function load_transformation_table()
+	local lua_script_file = string.sub(debug.getinfo(1, "S").source, 2)
+	local lua_script_directory = vim.fs.dirname(lua_script_file)
+	local normalized_directory = vim.fs.normalize(lua_script_directory)
+	local abbreviation_data_file = vim.fs.joinpath(
+		normalized_directory,
+		"../../abbreviation_data/abbreviations.json"
+	)
+
+	local file = io.open(abbreviation_data_file, "r")
+	if not file then
+		error(string.format("Unable to open file: %q", abbreviation_data_file))
+	end
+
+	local raw_table = vim.json.decode(file:read("*a"))
+	file:close()
+
+	for key, value in pairs(raw_table) do
+		local first_char = key:sub(1, 1)
+		local remaining_key = key:sub(2)
+
+		-- 1st map: to unicode
+		to_unicode_map[first_char] = to_unicode_map[first_char] or {}
+		to_unicode_map[first_char][remaining_key] = value
+
+		-- 2nd map: from unicode
+		from_unicode_map[value] = from_unicode_map[value] or {}
+		table.insert(from_unicode_map[value], key)
+	end
+
+	-- vim.print(vim.inspect(from_unicode_map))
+	-- nvim: use "ga" in normal mode to get info about the character under
+	--     the cursor
+end
 
 local function transform_text(text)
 	vim.print("Transforming: ", text)
@@ -30,6 +65,8 @@ function text2unicode.setup(opts_arg)
 	end, {
 		desc = "text2unicode: Transform abbreviations in current line",
 	})
+
+	load_transformation_table()
 end
 
 return text2unicode
