02dc6e09fc98e39c1e070ad71a763f7058fc5543
Bernhard Schweighofer <bernhard.schweighofer@tugraz.at>
2025-06-30 15:42:28 +0200

    Transformation of entries in single line works

--- lua/text2unicode/init.lua
+++ lua/text2unicode/init.lua
@@ -8,10 +8,8 @@ local function load_transformation_table()
 	local lua_script_file = string.sub(debug.getinfo(1, "S").source, 2)
 	local lua_script_directory = vim.fs.dirname(lua_script_file)
 	local normalized_directory = vim.fs.normalize(lua_script_directory)
-	local abbreviation_data_file = vim.fs.joinpath(
-		normalized_directory,
-		"../../abbreviation_data/abbreviations.json"
-	)
+	local abbreviation_data_file =
+		vim.fs.joinpath(normalized_directory, "../../abbreviation_data/abbreviations.json")
 
 	local file = io.open(abbreviation_data_file, "r")
 	if not file then
@@ -34,13 +32,61 @@ local function load_transformation_table()
 		table.insert(from_unicode_map[value], key)
 	end
 
-	-- vim.print(vim.inspect(from_unicode_map))
+	-- vim.print(vim.inspect(to_unicode_map))
 	-- nvim: use "ga" in normal mode to get info about the character under
 	--     the cursor
 end
 
 local function transform_text(text)
-	vim.print("Transforming: ", text)
+	local search_from = 1
+	local plain_search = true
+
+	local found_leader = 0
+
+	while true do
+		local start, stop = string.find(text, opts.leader, search_from, plain_search)
+		if not start then
+			break
+		end
+
+		-- get first character after the pattern-leader
+		local first_pattern_char = string.sub(text, stop + 1, stop + 1)
+		if not first_pattern_char then
+			-- no more characters available
+			break
+		end
+
+		found_leader = found_leader + 1
+
+		local longest_match = 0
+		local replacement_string
+		local possible_candidates = to_unicode_map[first_pattern_char]
+
+		if possible_candidates then
+			for key, value in pairs(possible_candidates) do
+				local keylength = string.len(key)
+				if key == string.sub(text, stop + 2, stop + 1 + keylength) then
+					if longest_match < (keylength + 1) then
+						longest_match = keylength + 1
+						replacement_string = value
+					end
+				end
+			end
+
+			if longest_match > 0 then
+				text = string.sub(text, 1, start - 1)
+					.. replacement_string
+					.. string.sub(text, stop + 1 + longest_match)
+			end
+		end
+
+		search_from = start + 1
+	end
+
+	if found_leader > 0 then
+		vim.print("Found " .. found_leader .. " leaders")
+	end
+
 	return text
 end
 
